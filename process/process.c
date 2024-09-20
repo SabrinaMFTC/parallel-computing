ac0d161bff4c747694bd8280b10c50f0f90f4c4f
Sabrina Midori <sabrina.mftc@gmail.com>
2024-09-20 19:55:43 -0300

    update process program

--- process/process.c
+++ process/process.c
@@ -1,3 +1,14 @@
+/* This program is written based on the code available at https://www.csl.mtu.edu/cs4411.ck/www/NOTES/process/shm/example-1.html.
+   It shows the communication between parent and child processes using shared memory.
+
+   The following explanation is also adapted from the code obtained in the link above:
+     The main function runs as a server and uses IPC_PRIVATE to request a private shared memory. Since the client is the server's child process created after the shared memory has been created and attached, the child client process will receive the shared memory in its address space and as a result no shared memory operations are required.
+     This program asks for a shared memory of five integers and attaches this shared memory segment to its address space. Pointer ShmPTR points to the shared memory segment.
+     Then, this program forks a child process to run function ClientProcess(). Thus, two identical copies of address spaces are created, each of which has a variable ShmPTR whose value is a pointer to the shared memory. As a result, the child process already knows the location of the shared memory segment and does not have to use shmget() and shmat().
+     The parent waits for the completion of the child. For the child, it retrieves the five integers, which were stored there by the parent before forking the child, sums the first four and store this value in the fifth integer, prints them and exits. The wait() system call in the parent will detect this.
+     The parent then multiplies the fifth integer value by 2 and finally exits.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -11,10 +22,10 @@ void  ClientProcess(int []);
 
 void  main(int  argc, char *argv[])
 {
-     int    ShmID;
-     int    *ShmPTR;
-     pid_t  pid;
-     int    status;
+     int   ShmID;
+     int   *ShmPTR;
+     pid_t pid;
+     int   status;
 
      if (argc != 5) {
           printf("Use: %s #1 #2 #3 #4\n", argv[0]);
@@ -83,6 +94,7 @@ void  ClientProcess(int SharedMem[])
      SharedMem[4] = SharedMem[0] + SharedMem[1] + SharedMem[2] + SharedMem[3];
 
      printf("*** Client found %d %d %d %d %d in shared memory\n",
-                SharedMem[0], SharedMem[1], SharedMem[2], SharedMem[3], SharedMem[4]);
+            SharedMem[0], SharedMem[1], SharedMem[2], SharedMem[3], SharedMem[4]);
+
      printf("*** Client is about to exit\n");
 }
